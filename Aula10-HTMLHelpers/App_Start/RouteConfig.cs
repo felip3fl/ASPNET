be0e67723326c2ab7d8c2786945140840c8a5964
Felipe Lima <felip3.fl@gmail.com>
2019-04-20 22:47:26 -0300

    Aula 10 - HTMLHelpers Usando o primeiro HTML Helpers

--- Aula10-HTMLHelpers/App_Start/RouteConfig.cs
+++ Aula10-HTMLHelpers/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Aula10_HTMLHelpers
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Cliente", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
