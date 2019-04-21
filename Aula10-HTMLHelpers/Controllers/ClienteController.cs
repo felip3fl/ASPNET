95fa204ed2703a3fb633a0190a4ac8dcd9a82437
Felipe Lima <felip3.fl@gmail.com>
2019-04-20 22:41:19 -0300

    Aula10-HTMLHelpers Part 1

---
+++ Aula10-HTMLHelpers/Controllers/ClienteController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Aula10_HTMLHelpers.Models;
+
+namespace Aula10_HTMLHelpers.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Detalhe()
+        {
+            //Logica de acesso aos dados usando entidades
+            //ado .net , entity framework, NHibernate
+
+            Cliente cliente = new Cliente()
+            {
+                CliendId = 200,
+                Nome = "Felipe",
+                Email = "felip3.fl@gmail.com",
+                Telefone = "11 6666-6666"
+            };
+
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult SalvarCliente(Cliente cli)
+        {
+            //Outra forma que pode ser feita para pegar os dados no ActionResult
+            //public ActionResult SalvarCliente(int CliendId, string Nome, string Email, string telefone)
+
+            return null;
+        }
+    }
+}
---
+++ Aula10-HTMLHelpers/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula10_HTMLHelpers.Models
+{
+    public class Cliente
+    {
+        //Este é meu modelo de dominio
+        public int CliendId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
