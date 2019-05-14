31085319181f7063d16f187ef10e6bbcd627d75d
Felipe Lima <felip3.fl@gmail.com>
2019-05-13 21:32:57 -0300

    Aula 23 - Usando varias tabelas - 2 Criar o controlador TipoCliente

---
+++ Aula12-EntityFramework/Controllers/TipoClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Aula12_EntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula12_EntityFramework.Controllers
+{
+    public class TipoClienteController : Controller
+    {
+        // GET: TipoCliente
+        public ActionResult Index()
+        {
+            using (ClienteContext clienteContext = new ClienteContext())
+            {
+                List<Tipo> tiposCliente = clienteContext.Tipos.ToList();
+                return View(tiposCliente);
+            }
+        }
+    }
+}
